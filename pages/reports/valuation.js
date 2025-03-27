4ac20caa482d56b3ada800509c8e76c337813d69
GROCERMAXX <muhammedcharfaray@gmail.com>
2025-03-27 22:54:44 +0200

    Update valuation.js

--- pages/reports/valuation.js
+++ pages/reports/valuation.js
@@ -1,1 +1,11 @@
-// Stock Valuation Report Page
+import React from 'react';
+
+const Valuation = () => {
+  return (
+    <div>
+      <h1>Valuation Page</h1>
+    </div>
+  );
+};
+
+export default Valuation;
