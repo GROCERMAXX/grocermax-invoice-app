552d70cd9c922586d175f3823b9a255a82e3f9d4
GROCERMAXX <muhammedcharfaray@gmail.com>
2025-03-27 21:50:47 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className='p-10 text-center'>
+      <h1 className='text-4xl font-bold mb-4'>Welcome to GrocerMax 🚀</h1>
+      <p className='text-lg'>Your accounting system is now live on Netlify!</p>
+    </div>
+  );
+}
