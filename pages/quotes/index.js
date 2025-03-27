e60b8e3c9278200f8ae47945c657eec9a7d7f92d
GROCERMAXX <muhammedcharfaray@gmail.com>
2025-03-27 22:51:24 +0200

    Update index.js

--- pages/quotes/index.js
+++ pages/quotes/index.js
@@ -1,1 +1,11 @@
-// Full Quote Generator Page
+import React from 'react';
+
+const Quotes = () => {
+  return (
+    <div>
+      <h1>Quotes Page</h1>
+    </div>
+  );
+};
+
+export default Quotes;
