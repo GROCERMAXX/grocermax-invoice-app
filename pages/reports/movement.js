a0aa2210a2665843995d1193daa3d833a17189f1
GROCERMAXX <muhammedcharfaray@gmail.com>
2025-03-27 22:52:20 +0200

    Update movement.js

--- pages/reports/movement.js
+++ pages/reports/movement.js
@@ -1,1 +1,11 @@
-// Item Movement Report Page
+import React from 'react';
+
+const Reports = () => {
+  return (
+    <div>
+      <h1>Reports Page</h1>
+    </div>
+  );
+};
+
+export default Reports;
