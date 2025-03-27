57a0b39adc989afa885ea765c6a9a50c1f3dc51a
GROCERMAXX <muhammedcharfaray@gmail.com>
2025-03-27 22:55:58 +0200

    Update index.js

--- pages/suppliers/index.js
+++ pages/suppliers/index.js
@@ -1,1 +1,11 @@
-// Full Supplier Invoice Page
+import React from 'react';
+
+const Suppliers = () => {
+  return (
+    <div>
+      <h1>Suppliers Page</h1>
+    </div>
+  );
+};
+
+export default Suppliers;
