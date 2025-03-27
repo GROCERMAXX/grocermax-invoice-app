a7173098ff0321ba69b1f628befed6412160fb4a
GROCERMAXX <muhammedcharfaray@gmail.com>
2025-03-27 16:16:55 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,8 @@
-export { default } from '../components/ui/InvoiceGenerator';
+export default function Home() {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1 style={{ fontSize: "2rem", fontWeight: "bold" }}>Welcome to GrocerMax 🚀</h1>
+      <p>Login to get started with invoicing, quotes, inventory and more.</p>
+    </div>
+  );
+}
