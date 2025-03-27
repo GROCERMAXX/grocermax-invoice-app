93ebcbbb3883f9b29ad04b4e8f778b6359ecf57f
GROCERMAXX <muhammedcharfaray@gmail.com>
2025-03-27 22:55:21 +0200

    Update index.js

--- pages/returns/index.js
+++ pages/returns/index.js
@@ -1,1 +1,11 @@
-// Full Credit Notes & Supplier Returns
+import React from 'react';
+
+const Returns = () => {
+  return (
+    <div>
+      <h1>Returns Page</h1>
+    </div>
+  );
+};
+
+export default Returns;
